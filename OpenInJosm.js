3c2fe91788640b91a750669fe8cfcdf24bf019a1
infeeeee <gyetpet@mailbox.org>
2020-02-18 19:22:07 +0100

    openinjsm bump 0.2

--- OpenInJosm.js
+++ OpenInJosm.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OpenInJosm
 // @namespace    https://github.com/infeeeee/userscripts
-// @version      0.1
+// @version      0.2
 // @description  Open selected element in Josm
 // @author       infeeeee
 // @match        *://*.openstreetmap.org/*
